73f9d2507e3eda8781d27b0851f2d0934edede2b
vitolist <vito.list2005@gmail.com>
2025-04-05 16:06:55 +0200

    config

--- config/config.php
+++ config/config.php
@@ -8,12 +8,21 @@ define('APP_NAME', 'CodeSensei');
 define('APP_VERSION', '1.0.0');
 define('APP_AUTHOR', 'Noa Turk i Vito List');
 
-
-// define('APPFOLDER', 'codesensei.xyz');
-define('APPFOLDER', 'testiranje.in');
-define('DB_HOST', '138.197.178.70');
-define('DB_USERNAME', 'codesensei-remote');
-define('DB_PASSWORD', 'W7K0wlgMEX.TrLfZ');
-define('DB_NAME', 'codesensei_eu');
-// define('BASE_PATH', 'https://codesensei.xyz');
-define('BASE_PATH', 'https://testiranje.in');
+if (DEVELOPMENT) {
+    define('APPFOLDER', 'codesensei');
+    define('DB_HOST', '138.197.178.70');
+    define('DB_USERNAME', 'codesensei-remote');
+    define('DB_PASSWORD', 'W7K0wlgMEX.TrLfZ');
+    define('DB_NAME', 'codesensei_eu');
+    // define('BASE_PATH', 'https://codesensei.xyz');
+    define('BASE_PATH', 'https://testiranje.in');
+} else {
+    // define('APPFOLDER', 'codesensei.xyz');
+    define('APPFOLDER', 'testiranje.in');
+    define('DB_HOST', '138.197.178.70');
+    define('DB_USERNAME', 'codesensei');
+    define('DB_PASSWORD', '14(4n;C5DH,|g5');
+    define('DB_NAME', 'codesensei_eu');
+    // define('BASE_PATH', 'https://codesensei.xyz');
+    define('BASE_PATH', 'https://testiranje.in');
+}
